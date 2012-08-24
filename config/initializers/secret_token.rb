4a92d39cce38ef41260182880bfb7afd3941c926
Дмитрий <fazid@fazid-Compaq-Presario-CQ71-Notebook-PC.(none)>
2012-08-24 21:14:08 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '52c2763622aa52cdfee125589caa63f079469ec78a1e1624393482f556e4b8a35c5100d06c8bccea2f298658cbbd1981e14b767b03fe2076beec1b4b76904b5e'
